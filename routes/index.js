7b733f967f3c3434164fac0411840f6b39ac24a3
toomanybugs1 <43858006+toomanybugs1@users.noreply.github.com>
2019-02-21 21:43:48 -0600

    Charts Added

--- routes/index.js
+++ routes/index.js
@@ -4,52 +4,74 @@ const exec = require('child_process').exec;
 var fs = require('fs');
 
 var bodyParser = require('body-parser');
+var Chart = require('chart.js')
 
 var jarPath = 'C:\\Users\\Trey\\Documents\\NodeJSPractice\\NodeApp\\myapp\\test.jar ';
+
+function parseProteins(inputTxt){
+    
+    var proteins = [];
+    
+    var proteinsText = inputTxt.split(">"); //first we split our sequences
+    
+    for (i = 1; i < proteinsText.length; i++) {
+        
+        var curProt = {
+            
+            head : "",
+            name: "",
+            entries : [],
+            
+        }
+        
+        var headBody = proteinsText[i].split('\n'); //separates head and body
+        var curEntries = headBody[1].split(' '); //splits our entries
+        
+        curProt.head = headBody[0];
+        curProt.name = curProt.head.substr(0, 6); 
+        curProt.entries = curEntries;
+        
+        proteins.push(curProt);
+    }
+
+    return proteins;
+    
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+    
 router.post('/', function(req, res) {
     
     if (!req.files.UPLOADFILE) { //no files
     
         var input = (req.body.QUERY).toString();
-    
-        //console.log(req.body);
-    
-        var childProcess = exec('java -jar ' + jarPath + "\"" + input + "\"", 
-            function(err, stdout, stderr) {
-                if (err) {
-                    console.log(err)
-                }
-                console.log(stdout)
-            });
-    
-        res.render('index', { title: 'Express' });
+        
+        var inputProteins = parseProteins(input);
+        
+        
+        res.render('output', {title: 'Express', proteins: inputProteins});
         
     }
     else {
         
         var inputFile = req.files.UPLOADFILE.data.toString('utf8');
         
-        var childProcess = exec('java -jar ' + jarPath + "\"" + inputFile + "\"", 
-            function(err, stdout, stderr) {
-                if (err) {
-                    console.log(err)
-                }
-                console.log(stdout)
-            });
-    
-        res.render('index', { title: 'Express' });
+        var inputProteins = parseProteins(inputFile);
+        
+        console.log("Entries: " + inputProteins.length);
+        
+        res.render('output', {title: 'Express', proteins: inputProteins});
         
     }
     
 });
+    
 
     
  
 
 module.exports = router;
+//module.exports = inputProteins;
